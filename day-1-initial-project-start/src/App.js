30c00e781d8f19cbc025b1d9804cfc90fbe1fa5f
ClaesF0 <claes_folkestad@hotmail.com>
2023-03-16 13:12:27 +0100

    listed and basic markup first page

--- day-1-initial-project-start/src/App.js
+++ day-1-initial-project-start/src/App.js
@@ -16,16 +16,17 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="mx-auto">
+      <div className="flex flex-wrap mx-auto">
         {products.map((product) => (
-          <div className="mx-auto ">
-            <hgroup
-              style={{ backgroundImage: `url(${product.images[0]})` }}
-              className="bg-fit  h-[350px] w-[300px] basis-1/4 border-2 border-gray-800 max-w-1/2 sm:max-w-1/3 flex flex-col m-2 p-2"
-            >
+          <div className="mx-auto  ">
+            <hgroup className=" h-[350px] w-[300px] basis-1/4 border-2 border-gray-800 max-w-1/2 sm:max-w-1/3 flex flex-col m-2 p-2">
               <span className="text-gray-700 text-xl mx-auto">
                 {product.title}
               </span>
+              <img
+                className="max-w-[200px] mx-auto bg-cover "
+                src={product.images[0]}
+              />
             </hgroup>
           </div>
         ))}
