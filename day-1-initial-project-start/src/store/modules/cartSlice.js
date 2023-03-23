2e4030ed50af1c1baaeafa6bddd0ce0b548966b6
ClaesF0 <claes_folkestad@hotmail.com>
2023-03-23 14:37:37 +0100

    cart remove function

--- day-1-initial-project-start/src/store/modules/cartSlice.js
+++ day-1-initial-project-start/src/store/modules/cartSlice.js
@@ -15,6 +15,14 @@ const cartSlice = createSlice({
       state.numberOfProductsInCart = state.productsInCart.length;
       console.log("state.productsInCart.length", state.productsInCart.length);
     },
+    REMOVE_PRODUCT_FROM_CART: (state, action) => {
+      console.log("state", state);
+      console.log("action", action);
+      state.productsInCart = state.productsInCart.filter(
+        (product) => product.id !== action.payload
+      );
+      state.numberOfProductsInCart = state.productsInCart.length;
+    },
   },
 });
 
@@ -22,8 +30,14 @@ export default cartSlice.reducer;
 
 //actions
 const { ADD_PRODUCT_TO_CART } = cartSlice.actions;
+const { REMOVE_PRODUCT_FROM_CART } = cartSlice.actions;
 
 export const addSingleProductToCart = (productData) => (dispatch) => {
   console.log("productData", productData);
   dispatch(ADD_PRODUCT_TO_CART(productData));
+};
+
+export const removeSingleProductFromCart = (productId) => (dispatch) => {
+  console.log("productId", productId);
+  dispatch(REMOVE_PRODUCT_FROM_CART(productId));
 };
